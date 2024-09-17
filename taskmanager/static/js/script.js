1cc0b08f99667367b8a9f25964c49053f491162f
Irina Bogdanova <irinabogdanova261@gmail.com>
2024-09-17 02:51:45 +0000

    Add modal to answer consent to deletion

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -13,4 +13,8 @@ document.addEventListener("DOMContentLoaded", function() {
     // select initialization
     let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
+
+    // modal initialization
+    let modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
 });
